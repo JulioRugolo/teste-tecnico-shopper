75195d773d974550eea2a676f70436968f6f788a
julio.rugolo <juliorugolo@gmail.com>
2024-11-26 09:24:12 -0300

    .gitignore

--- backend/dist/routes/driverRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const driverController_1 = require("../controllers/driverController");
-const router = (0, express_1.Router)();
-router.get('/', driverController_1.fetchAllDrivers);
-// get driver by id
-router.get('/:id', driverController_1.fetchDriverById);
-exports.default = router;
